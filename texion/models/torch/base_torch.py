4324a8d8cc0679127847ac9e25d14f0a390b0648
bharath g s <royalkingpin@gmail.com>
2018-09-29 13:49:46 +0530

    adds initial yoon kim model support.

--- texion/models/torch/base_torch.py
+++ texion/models/torch/base_torch.py
@@ -1,11 +1,29 @@
-"""Base pytorch based deep classifiers
+"""
+
+Base pytorch based deep classifiers
+
 """
 import torch
+from .yoonkim import YoonKimCnn
+
+
+OPTIONS = {"yoonkim": YoonKimCnn}
 
 
 class BaseTorch:
-    def __init__(self, name=None, params=None):
-        self.name = name
-        self.params = params
-        if name:
-            raise NotImplementedError("Work in Progress")
+    def __new__(cls, name, params=None):
+
+        if name not in OPTIONS.keys():
+            raise NameError(
+                f"please select one of these as the name: {[x for x in OPTIONS.keys()]}")
+
+        if params:
+            clf = OPTIONS.get(name)(**params)
+            print(
+                f"""classification model configured to use {clf.__class__.__name__} algorithm with parameters:\n{params}""")
+
+        else:
+            clf = OPTIONS.get(name)()
+            print(
+                f"""classification model configured to use {clf.__class__.__name__} algorithm.\nnote: running with default configuration""")
+        return clf
