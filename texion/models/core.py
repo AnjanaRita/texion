98c9c3942f18f3667da9aa031dc221994e9eab65
bharath g s <royalkingpin@gmail.com>
2018-10-03 15:00:34 +0530

    add docstring

--- texion/models/core.py
+++ texion/models/core.py
@@ -4,6 +4,27 @@ from .torch.base_torch import BaseTorch
 
 class Texion:
     def __new__(cls, mode, name, params=None):
+        """
+
+        parameters: 
+        __________
+
+        mode: str
+            configures Texion's Backend, choose either `Sklearn` or `Torch`.
+
+        name: str
+            configures the algorithm/architecture to be used for classification
+
+        params: dict
+            optional, a dictionary containing the model hyperparameters
+
+        returns: 
+        _______
+
+            Texion text classifier
+
+
+        """
         if mode == "Sklearn":
             print(f"configured to run with {mode} Backend")
             return BaseSklearn(name, params)
