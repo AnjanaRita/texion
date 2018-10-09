d9c2c7b0c6507dcd20addf7a9851aef76f8367c3
bharath g s <royalkingpin@gmail.com>
2018-10-09 15:40:58 +0530

    ...

--- texion/utils/general.py
+++ texion/utils/general.py
@@ -1,7 +1,10 @@
 from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer, HashingVectorizer, TfidfVectorizer
 
 
 def train_test_split_data(texts, labels, test_size=0.2, **kwargs):
     train_texts, test_texts, train_labels, test_labels = train_test_split(
         texts, labels, test_size=test_size, **kwargs)
     return train_texts, test_texts, train_labels, test_labels
+
+
